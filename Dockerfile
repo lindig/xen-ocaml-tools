c64e37a459f309ecfafeedc2139e4f2781d5cf4d
Christian <christian.lindig@citrix.com>
2017-07-18 10:43:06 +0100

    Cleanup: improve quoting, avoid cloning jbuilder
    Signed-off-by: Christian <christian.lindig@citrix.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,2 @@
-#
-
 FROM ocaml/ocaml:debian-9
-
 RUN apt-get install -y m4 libxen-dev libxen-dev libsystemd-dev
--- travis.sh
+++ travis.sh
@@ -1,12 +1,11 @@
-#
 
-uid=$(id -u)
-gid=$(id -g)
+uid="$(id -u)"
+gid="$(id -g)"
 
-docker run --rm -u "$uid:$gid" -i -v $PWD:/mnt xen/ocaml <<'EOF'
+docker run --rm -u "$uid:$gid" -i -v $PWD:/mnt xen/ocaml <<-'EOF'
+  set -e
   cd /mnt
-  rm -rf jbuilder
-  git clone https://github.com/janestreet/jbuilder
+  test -d jbuilder || git clone https://github.com/janestreet/jbuilder
   make -C jbuilder
   ./jbuilder/_build/install/default/bin/jbuilder build
 EOF
