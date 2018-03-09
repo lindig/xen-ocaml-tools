942792d1fbf1e4ab0c214bad4b3ecad86aa606bb
Christian Lindig <christian.lindig@citrix.com>
2018-03-09 22:37:46 +0000

    Update travis.sh
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- travis.sh
+++ travis.sh
@@ -1,8 +1,5 @@
 
-uid="$(id -u)"
-gid="$(id -g)"
-
-docker run --rm -u "$uid:$gid" -i -v $PWD:/mnt lindig/xen-tools<<-'EOF'
+docker run --rm -i -v $PWD:/mnt lindig/xen-tools<<-'EOF'
   set -e
   cd /mnt
   make
